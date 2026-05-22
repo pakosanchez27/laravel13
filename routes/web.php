3f05730b5d1c6240636eaf15669eef3293b88ce2
Francisco Sánchez <javier.sanchezjps27@gmail.com>
2026-05-22 17:52:31 -0600

    clase 18 creacion de controlladores

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/auth/register', function () {
-    return view('auth.register');
-})->name('register');
+Route::get('/auth/register', [RegisterController::class, 'index'])->name('register');
 
-Route::get('/auth/login', function () {
-    return view('auth.login');
-})->name('login');
+Route::get('/auth/login', [LoginController::class, 'index'])->name('login');
